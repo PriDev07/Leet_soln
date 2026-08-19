6724f18815e5486e0502616914a18700aaf1ac7c
P.Lohani <103742912+PriDev07@users.noreply.github.com>
2026-08-19 21:15:29 +0530

    Time: 163 ms (5.09%) | Memory: 54.4 MB (5.15%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,23 @@
+bool comp(vector<int>a,vector<int>b){
+    return a[1]>b[1];
+}
+class Solution {
+public:
+
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),comp);
+        int ans=0;
+        for(int i=0;i<boxTypes.size();i++){
+            if(boxTypes[i][0]<truckSize){
+                ans+=boxTypes[i][0]*boxTypes[i][1];
+                truckSize-=boxTypes[i][0];
+                cout<<ans;
+            }else{
+                ans+=truckSize*boxTypes[i][1];
+                cout<<ans;
+                break;
+            }
+        }
+        return ans;
+    }
+};
