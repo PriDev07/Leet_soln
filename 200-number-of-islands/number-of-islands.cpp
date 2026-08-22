b15c0fe59005957fa4d452cf7177497b589f50e0
P.Lohani <103742912+PriDev07@users.noreply.github.com>
2026-08-22 10:34:31 +0530

    Time: 28 ms (48.47%) | Memory: 17.4 MB (51.53%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>> &visited, int row, int col){
-        int n = grid.size();
-        int m = grid[0].size();
-        int sr[] = {-1,1,0,0};
-        int sc[] = {0,0,-1,1};
-        queue<pair<int,int>>q;
-        q.push({row,col});
-        visited[row][col]=1;
-        while(!q.empty()){
-            auto [r,c]= q.front();
-            q.pop();
-            for(int k=0;k<4;k++){
-            int rowCheck = r+sr[k];
-            int colCheck = c+sc[k];
-            if(rowCheck>=0 && rowCheck <n && colCheck>=0 && colCheck<m && !visited[rowCheck][colCheck] && grid[rowCheck][colCheck]=='1'){
-                visited[rowCheck][colCheck]=1;
-                q.push({rowCheck,colCheck});
-            }
-        }
-        }
+    void solve(vector<vector<char>>& grid,vector<vector<int>>& visited, int i, int j){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
+    return;
+        if(grid[i][j]=='0') return;
+        if(visited[i][j]) return;
+        visited[i][j]=1;
+        solve(grid,visited,i-1,j);
+        solve(grid,visited,i+1,j);
+        solve(grid,visited,i,j-1);
+        solve(grid,visited,i,j+1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans =0;
-        vector<vector<bool>>visited(n,vector<bool>(m,0));
+        int ans=0;
+        vector<vector<int>>visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(grid,visited,i,j);
-                    ans++;
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    ans ++;
+                    solve(grid,visited,i,j);
                 }
             }
         }
