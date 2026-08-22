0726e60793c8085f492ea5f07e9b79d02ef0a8eb
P.Lohani <103742912+PriDev07@users.noreply.github.com>
2026-08-22 21:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (19.04%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int f(int idx,vector<int>&nums,vector<int>&dp){
+    int solve(vector<int>& nums, int idx,vector<int>& dp){
         if(idx==0) return nums[0];
         if(idx<0) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int pick = nums[idx]+f(idx-2,nums,dp);
-        int notPick=0+f(idx-1,nums,dp);
-        return dp[idx]=max(pick,notPick);
+        int left = nums[idx]+solve(nums,idx-2,dp);
+        int right = solve(nums,idx-1,dp);
+        return dp[idx]=max(left,right);
     }
     int rob(vector<int>& nums) {
+         vector<int>temp1,temp2;
          int n = nums.size();
          if(n==1) return nums[0];
-         vector<int>temp1,temp2;
          vector<int>dp1(n-1,-1),dp2(n-1,-1);
          for(int i=0;i<n;i++){
             if(i!=0) temp1.push_back(nums[i]);
             if(i!=n-1) temp2.push_back(nums[i]);
          }
-         int n1=temp1.size();
-         int n2=temp2.size();
-         return max(f(n1-1,temp1,dp1),f(n2-1,temp2,dp2));
+         return max(solve(temp1,n-2,dp1),solve(temp2,n-2,dp2));
     }
 };
