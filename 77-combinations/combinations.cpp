a5fef38709ac6f8b070c0192a86de7847829fbb2
P.Lohani <103742912+PriDev07@users.noreply.github.com>
2026-09-15 09:30:26 +0530

    Time: 39 ms (65.64%) | Memory: 62.5 MB (71.78%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void solve(int n, int k,vector<vector<int>>& ans,vector<int>&curr,int idx){
-        if(curr.size()==k){
-            ans.push_back(curr);
-            return;
-        }
-        for(int i=idx;i<=n;i++){
-            curr.push_back(i);
-            solve(n,k,ans,curr,i+1);
-            curr.pop_back();
-        }
+void solve(int n , int k, int i,vector<vector<int>>& ans, vector<int>& curr){
+    if(k==0){
+        ans.push_back(curr);
+        return;
     }
+    for(int x=i;x<=n;x++){
+        curr.push_back(x);
+        solve(n,k-1,x+1,ans,curr);
+        curr.pop_back();
+    }
+}
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
         vector<int>curr;
-        solve(n,k,ans,curr,1);
+        solve(n,k,1,ans,curr);
         return ans;
     }
 };
